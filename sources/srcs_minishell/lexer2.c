77a4e70e75fbdc5c92c93c3b5cef60869da75748
Melody DE SOUSA <mede-sou@e1r12p9.clusters.42paris.fr>
2022-10-31 12:01:40 +0100

    modif melo strncpy avec guillemets

--- sources/srcs_minishell/lexer2.c
+++ sources/srcs_minishell/lexer2.c
@@ -6,40 +6,23 @@
 /*   By: mede-sou <mede-sou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:54:29 by mede-sou          #+#    #+#             */
-/*   Updated: 2022/10/31 12:01:13 by mede-sou         ###   ########.fr       */
+/*   Updated: 2022/10/31 10:36:12 by mede-sou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/*prio ' ' et " " : verifier si fermes, sinon erreur
+/*prio ' ' et " " : verifier si fermes, sinon erreur OK
 chaque maillon = 1 mot (split espace)
 types :
 1 = cmd
 2 = pipe
-3 = redirection entree <
-4 = redirection sortie >
+3 = redirection entree < OK 
+4 = redirection sortie > OK
 5 = redirection << (append) doit recevoir un delimiteur
 6 = redirection sortie append (reecrit dessus) >>
 7 = var environnement $var
 */
-
-int	ft_append(t_ms **lex, char *str)
-{
-	char	*temp;
-	int		i;
-
-	i = 2;
-	while (str[i] == ' ')
-		i++;
-	while ((str[i] != ' ') && (str[i] != '\0') && (str[i] != '<') && (str[i] != '>'))
-		i++;
-	temp = ft_substr(str, 0, i);
-	printf ("temp = %s\n", temp);
-	ft_lstadd_back_ms(lex, ft_lstnew_ms(temp));	
-	return (1);
-}
-
 
 int	ft_check_quotes(char *str, char c)
 {
@@ -58,31 +41,21 @@ int	ft_check_quotes(char *str, char c)
 int	ft_chevron(t_ms **lex, char *str, char c)
 {
 	int		i;
-	int 	j;
 	char	*temp;
 
 	i = 1;
-	j = 1;
 	(void)c;
-	if (str[i] == c)
-	{
-		ft_append(lex, str);
-		return(0);
-	}
-	else
-	{
+	// if (str[i + 1] == c)
+		// ft_append();
+	// else
+	// {
 		while (str[i] == ' ')
 			i++;
 		while ((str[i] != ' ') && (str[i] != '\0') && (str[i] != '<') && (str[i] != '>'))
-		{
-			j++;
 			i++;
-		}
-		if (j == 1)
-			return (-1);
 		temp = ft_substr(str, 0, i);
-		ft_lstadd_back_ms(lex, ft_lstnew_ms(temp));		
-	}
+		ft_lstadd_back_ms(lex, ft_lstnew_ms(temp));
+	// }
 	return (1);
 	
 }
@@ -105,17 +78,8 @@ int	ft_lexer(t_ms *lex, char *str)
 			ft_lstadd_back_ms(&lex, ft_lstnew_ms(temp));
 			i += j + 1;
 		}
-		else if (str[i] == '<' || str[i] == '>')
-		{
-			
-			if ((ft_chevron(&lex, str + i, str[i])) == 0)
-				i++;
-			if ((ft_chevron(&lex, str + i, str[i])) == -1)
-			{	
-				printf ("syntax error near unexpected token '%c'\n", str[i]);
-				return (0);
-			}
-		}
+		if (str[i] == '<' || str[i] == '>')
+			ft_chevron(&lex, str + i, str[i]);
 		i++;
 	}
 	ft_view_lst(lex);
