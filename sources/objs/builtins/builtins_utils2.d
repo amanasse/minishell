148be6b24ff687a8899f1dd319467319e93493e0
Melody DE SOUSA <mede-sou@e1r12p9.clusters.42paris.fr>
2022-11-21 18:38:27 +0100

    test avec fonction export avec les fd recuperes au moment de la crea du tableau de cmd mais n'ecrit pas dans le fichier

--- sources/objs/builtins/builtins_utils2.d
+++
@@ -1,4 +0,0 @@
-sources/objs/builtins/builtins_utils2.o: \
-  sources/srcs_minishell/builtins/builtins_utils2.c includes/minishell.h \
-  includes/parsing.h includes/builtins.h \
-  includes/../sources/libft/includes/libft.h includes/execution.h
--- sources/objs/parsing/main.d
+++
@@ -1,3 +0,0 @@
-sources/objs/parsing/main.o: sources/srcs_minishell/parsing/main.c \
-  includes/minishell.h includes/parsing.h includes/builtins.h \
-  includes/../sources/libft/includes/libft.h includes/execution.h
