7578b36fd6b4350a6f252ea747fd6b37ac20805e
Melody DE SOUSA <mede-sou@e1r12p11.clusters.42paris.fr>
2022-11-22 17:52:22 +0100

    chevron marche pas

--- sources/objs/parsing/clean_quotes.d
+++
@@ -1,4 +0,0 @@
-sources/objs/parsing/clean_quotes.o: \
-  sources/srcs_minishell/parsing/clean_quotes.c includes/minishell.h \
-  includes/parsing.h includes/builtins.h \
-  includes/../sources/libft/includes/libft.h includes/execution.h
